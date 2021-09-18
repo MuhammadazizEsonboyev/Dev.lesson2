b1f8579f4246ae9a51fa13f2ae15984a95aea65c
esonboyev03@bk.ru <esonboyev03@gmail.com>
2021-09-18 12:52:01 +0000

    lesson2

---
+++ 2 dars c++.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main (){
+	int mening_raqamim = 20;
+	int sening_raqaming = 10;
+	
+	int yigindi;
+	
+	yigindi = mening_raqamim + sening_raqaming;
+	
+	printf(" mening_raqamim : %d \n sening_raqaming : %d \n yigindi : %d ",  mening_raqamim, sening_raqaming , yigindi );
+	return 0;
+}
+
+
+// Compilyator bu bironbir dasturlash tiliga o'girib beradi  masalan || 010101
+// int funksiya turi.
+//printf ekranga chiqaruvchi funksya.
+// o'zgaruvchilarni chiqarish uchun int turida %d ishlatilinadi.
+
